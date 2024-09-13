de988220c49f77793781e611cc61535625b7d4f1
Taurai. M <rusiqe@gmail.com>
2024-09-13 10:58:16 +0200

    update main

--- src/main.py
+++ src/main.py
@@ -23,4 +23,4 @@ if __name__ == '__main__':
         logger.warning("No domains were synced. Check your API credentials and internet connection.")
 
     logger.info("Starting the web application...")
-    app.run(debug=True)    app.run(debug=True)
+    app.run(debug=True)
